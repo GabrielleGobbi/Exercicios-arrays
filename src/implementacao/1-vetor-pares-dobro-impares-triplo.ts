0ad7a8f2e0576900eb6493260c2c1b5553e73adc
GessicaGobbi <gessicagobbi@hotmail.com>
2025-05-02 17:53:03 -0300

    arrumado

--- src/implementacao/1-vetor-pares-dobro-impares-triplo.ts
+++ src/implementacao/1-vetor-pares-dobro-impares-triplo.ts
@@ -1,14 +1,13 @@
 function gerarVetorParesDobroImparesTriplo(numberList:number[]) :number [] {
-    //Implemente um método que leia um vetor de 30 números 
-    if(numberList[30]!==undefined){//se for diferente de um arrey de 30 de tamanho
-        throw new Error( "O Array é maior que 30 ");//throw = lançar uma exceção (erro) , seria legal usar o try catch pra tratar, mas não usaremos por enquanto 
+    if(numberList[30]!==undefined){
+        throw new Error( "O Array é maior que 30 ");
     }
     let numberListUpdate :number[] = [];
-    for(let i=0; i<30;i++){//inicia o laço na posição 0 , e roda enquanto i for < que 30 , a cada rodada ela incrementa 1 em i
-        if(i%2===0){// par é divisivel por 2 sem resto 
-            numberListUpdate[i]=numberList[i] *2;//queremos pegar o valor que esta no indice i e multiplicar por 2
+    for(let i=0; i<30;i++){
+        if(i%2===0){
+            numberListUpdate[i]=numberList[i] *2;
         }
-        else{ // se não for par , ele é impar , para um numero ser impar ele quando dividido por 2 vai ter resto , por exemplo numeros primos como o 7 e 13 são impar 
+        else{ 
             numberListUpdate[i]=numberList[i]*3 //
         }
 
@@ -18,13 +17,3 @@ function gerarVetorParesDobroImparesTriplo(numberList:number[]) :number [] {
 module.exports=gerarVetorParesDobroImparesTriplo;
 
 console.log(gerarVetorParesDobroImparesTriplo([1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,21,22,23,24,25,26,27,28,29,30]) )
-/**O problema:
-
-Implemente um método que leia um vetor de 30 números inteiros e gere um segundo vetor. As posições pares devem ser o dobro do vetor original, e as ímpares, o triplo.
-
-O método deve percorrer os 30 números inteiros do vetor.
-Se a posição for par, o valor será multiplicado por 2.
-Se a posição for ímpar, o valor será multiplicado por 3.
-Exemplo:
-Quando a entrada for [1, 2, 3, 4, 5], a saída deve ser [3, 4, 9, 8, 15].
- */
--- src/implementacao/2-vetores-positivos-negativos.ts
+++ src/implementacao/2-vetores-positivos-negativos.ts
@@ -1,5 +1,5 @@
 function separarPositivosNegativos(arrayNumber:number[]) : {arrayPositiveNumber:number[] ,arrayNegativeNumber:number[]} {
-    //primeiro verificar o tamanho do array
+    
     if(arrayNumber.length!==8){
         throw new Error("O array é diferente do tamanho pedido que é 8");
     }
@@ -8,8 +8,8 @@ function separarPositivosNegativos(arrayNumber:number[]) : {arrayPositiveNumber:
     const arrayNegativeNumber:number[] = [];
   
     for(let i=0;i<8;i++){
-        if (arrayNumber[i]>0) {//alguma coisa for positiva
-            arrayPositiveNumber.push(arrayNumber[i]);//quero o valor da posição
+        if (arrayNumber[i]>0) {
+            arrayPositiveNumber.push(arrayNumber[i]);
         } else {
             arrayNegativeNumber.push(arrayNumber[i]);
         }
