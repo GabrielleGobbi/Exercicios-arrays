f4566e38cc610db6fc411aff456a5faf3528a1be
GessicaGobbi <gessicagobbi@hotmail.com>
2025-05-02 17:47:03 -0300

    7

--- src/implementacao/7-pontuacao-concurso.ts
+++ src/implementacao/7-pontuacao-concurso.ts
@@ -22,13 +22,3 @@ const candidatos: person[] = [
 ];
 
 console.log(listarAprovados(candidatos));
-/**
- * Crie um método que leia a pontuação final de 25 provas de um concurso e os nomes dos respectivos participantes. O método deve retornar os participantes que obtiveram mais de 70 pontos.
-
-O método deve percorrer as pontuações e listar os nomes dos participantes que atingiram mais de 70 pontos.
-Exemplo:
-Quando a entrada for:
-Nome: João
-Pontuação: 75
-A saída deve ser João obteve mais de 70 pontos.
- */
